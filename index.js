56565074c0ca1aaaf08e227491019748e1ae8ee2
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-05-21 10:58:17 -0700

    refactored array component into a function

--- index.js
+++ index.js
@@ -2,22 +2,29 @@ console.log("welcome to Rock, Paper, Scissors");
 
 
 //returning either rock, paper or scissors through an RNG
-const computerPlay = ["rock", "paper", "scissors"]
-
-const computerChoice = computerPlay[Math.floor(Math.random() * computerPlay.length)];
-
+function computerPlay(){
+ const computerArray = ["rock", "paper", "scissors"] 
+const computerChoice = Math.floor(Math.random() * computerArray.length);
 console.log(computerChoice)
+return computerArray[computerChoice]
+} 
+
+
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection
-    computerSelection
+    if (computerSelection.toLowerCase() === playerSelection.toLowerCase()){
+      console.log("Its a draw!!!");
+  //   }else if{
 
-  }
+  //   }else if{
+
+  //   }
+        
+  //     }
+  //   }
+    }
+ }
   
   const playerSelection = "rock";
   const computerSelection = computerPlay();
   console.log(playRound(playerSelection, computerSelection));
-
-//   function game(playRound){
-     
-//   }
