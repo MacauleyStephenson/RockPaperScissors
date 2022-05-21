bb658842de8ed6b3188ebcf760067b9879e0f2cc
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-05-21 05:19:05 -0700

    refactored vars to more updated consts

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 console.log("welcome to Rock, Paper, Scissors");
 
 
-//returning either rock, paper or scissors
-var computerPlay = ["rock", "paper", "scissors"]
+//returning either rock, paper or scissors through an RNG
+const computerPlay = ["rock", "paper", "scissors"]
 
-var computerChoice = computerPlay[Math.floor(Math.random() * computerPlay.length)];
+const computerChoice = computerPlay[Math.floor(Math.random() * computerPlay.length)];
 
 console.log(computerChoice)
 
 function playRound(playerSelection, computerSelection) {
-    // your code here!
+    playerSelection
+    computerSelection
+
   }
   
   const playerSelection = "rock";
   const computerSelection = computerPlay();
   console.log(playRound(playerSelection, computerSelection));
 
-  function game(playRound){
+//   function game(playRound){
      
-  }
+//   }
