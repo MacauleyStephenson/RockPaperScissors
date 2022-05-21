91d356ee86d37700c1e9fc97719de6bb8c7a5915
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-05-21 04:39:51 -0700

    adding js boilerplate

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("welcome to Rock, Paper, Scissors");
+
+function computerPlay(){
+
+}
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+  }
+  
+  const playerSelection = "rock";
+  const computerSelection = computerPlay();
+  console.log(playRound(playerSelection, computerSelection));
+
+  function game(playRound){
+     
+  }
