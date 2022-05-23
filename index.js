244b8b17ff24a6a69c9b59947c3e9d4e602e0692
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-05-23 10:06:42 -0700

    added max amount of rounds played per match

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-console.log("welcome to Rock, Paper, Scissors");
+console.log("Welcome to Rock, Paper, Scissors");
 
 
 //returning either rock, paper or scissors through an RNG
 function computerPlay(){
- const computerArray = ["rock", "paper", "scissors"] 
+const computerArray = ["rock", "paper", "scissors"] 
 const computerChoice = Math.floor(Math.random() * computerArray.length);
 console.log(computerChoice)
 return computerArray[computerChoice];
 } 
 
+//Player input
 function playerPlay(){
-  console.log("Do you choose ")
+  console.log("Do you choose Rock(1), Paper(2) or Scissors(3)");
+  let playerChoice = prompt();
 }
 
+//Play a round
 function playRound(playerSelection, computerSelection) {
     if (computerSelection.toLowerCase() === playerSelection.toLowerCase()){
       console.log("Its a draw!!!");
@@ -23,3 +26,11 @@ function playRound(playerSelection, computerSelection) {
   const playerSelection = "rock";
   const computerSelection = computerPlay();
   console.log(playRound(playerSelection, computerSelection));
+
+
+  // Takes amount of max rounds to be played per 1 match
+  const playGame = () => {
+    for (let i = 0; i < 5; i++) {
+      playRound();
+   }
+  }
