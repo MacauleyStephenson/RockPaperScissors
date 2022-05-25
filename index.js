212476a5ee61e0ecc2008d0ef6bc0ffdd476fd0c
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-05-25 03:42:37 -0700

    refactored code

--- index.js
+++ index.js
@@ -1,68 +1,50 @@
-console.log("Welcome to Rock, Paper, Scissors");
+alert("Welcome to rock, paper, scissors")
 
+const playerSelection = prompt("Choose your hand ");
+console.log("You have chosen " + playerSelection)
 
-//returning either rock, paper or scissors through an RNG
 function computerPlay(){
-const computerArray = ["rock", "paper", "scissors"] 
-const computerChoice = Math.floor(Math.random() * computerArray.length);
-return computerArray[computerChoice];
-} 
+  const aiChoice = ["rock", "paper", "scissors"]
 
-//Player input
-function playerPlay(){
-  console.log("Do you choose Rock(1), Paper(2) or Scissors(3) ?");
-  let playerChoice = prompt("Do you choose Rock(1), Paper(2) or Scissors(3) ?");
-  if (playerChoice.toLowerCase() === "rock" || parseInt(playerChoice) === 1){ 
-    playerChoice = 'rock'
-  }else if(playerChoice.toLowerCase() === "paper" || parseInt(playerChoice) === 2){
-    playerChoice = 'paper'
-  }else if(playerChoice.toLowerCase() === "scissors" || parseInt(playerChoice) === 3){
-    playerChoice = 'scissors'
-} else if(playerChoice.toLowerCase() !== "rock" || "scissors" ||"paper" || parseInt(playerChoice !== 1 || 2 || 3))  {
-alert("PLease enter your selection again")
-playerSelection();
-}else{
-  console.log("Not sure what you're doing, try again");
-}
-return playerChoice;
+//returns from starting array
+ return RNG = Math.floor(Math.random() * aiChoice.length)
 }
 
- const playerSelection = playerPlay();
-  const computerSelection = computerPlay();
+//Calls AI for choice
+const computerSelection = computerPlay()
+console.log("Ai has chosen " + computerPlay())
 
-//Play a round
-function playRound(playerSelection, computerSelection) {
-    if (computerSelection.toLowerCase() === playerSelection.toLowerCase()){
-      console.log("Its a draw!!!");
-    return("Your choice is " + playerSelection + ". " + "Computer: " + computerSelection);
-    }else if(computerSelection.toLowerCase() === "rock" && playerSelection.toLowerCase() === "scissors" ){
-      console.log("Rock beats scissors, you loose! ") 
-      return("Your choice: " + playerSelection + ". " + "Computer: " + computerSelection);
-    }else if(computerSelection.toLowerCase() === "scissors" && playerSelection.toLowerCase() === "rock"){
-      console.log("Rock beats scissors, you won!")
-      return("Your choice: " + playerSelection + ". " + "Computer: " + computerSelection);
-    }else if(computerSelection.toLowerCase() === "scissors" && playerSelection.toLowerCase() === "paper"){
-    console.log("scissors beats paper, you loose!")
-    return("Your choice: " + playerSelection + ". " + "Computer: " + computerSelection);
-    }else if(computerSelection.toLowerCase() === "rock" && playerSelection.toLowerCase() === "paper"){
-      console.log("paper beats rock, you win!")
-    return("Your choice: " + playerSelection + ". " + "Computer: " + computerSelection);
-    }else if(computerSelection.toLowerCase() === "paper" && playerSelection.toLowerCase() === "rock"){
-      console.log("paper beats rock, you loose!")
-      return("Your choice: " + playerSelection + ". " + "Computer: " + computerSelection);
-    }else if(computerSelection.toLowerCase() === "paper" && playerSelection.toLowerCase() === "scissors"){
-      console.log("scissors beats paper, you won!")
-      return("Your choice: " + playerSelection + ". " + "Computer: " + computerSelection);
-    } else {
-      console.log("Something went wrong, please try again!");
+
+ //Plays one round of the game
+ function playRound(playerSelection, computerSelection){
+   if(playerSelection === computerSelection){
+    return "Tie!";
+   }else if(playerSelection === "paper"){
+     if(computerSelection === "rock"){
+       return "You win"
+     }else{
+       return "You loose"
+     }
+     }else if(playerSelection === "rock"){
+       if(computerSelection === "scissors"){
+         return "You win"
+       }else{
+         return "You loose"
+       }
+        }else if (playerSelection === "scissors"){
+            if (computerSelection === "paper"){
+                return "Win";
+            } else {
+                return "Loss";
+            }
+        } 
     }
-  }
- 
-  function game() {
-    //Play game 5 times
-    for (let i = 0; i < 5; i++) {
-      // Call playRound function, passing in newly returned values
-      // from the playerPlay and computerPlay functions and log to console
-      console.log(playRound(playerPlay(), computerPlay()));
-    }
- }
+
+        
+
+
+//returns variables to play the game
+playRound(playerSelection, computerSelection)
+computerPlay()
+
+console.log(playRound(playerSelection, computerSelection));
