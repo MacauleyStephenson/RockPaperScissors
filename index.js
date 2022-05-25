68fdff3163ce29b8ddd9875aa5a4d3e023e279a6
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-05-25 04:30:44 -0700

    refactored logic to previous version due to inconsisencies

--- index.js
+++ index.js
@@ -1,45 +1,46 @@
 alert("Welcome to rock, paper, scissors")
 
-const playerSelection = prompt("Choose your hand ");
-console.log("You have chosen " + playerSelection)
+const playerSelection = prompt("Please choose rock (1), paper (2) or scissors (3)");
 
 function computerPlay(){
   const aiChoice = ["rock", "paper", "scissors"]
 
 //returns from starting array
- return RNG = Math.floor(Math.random() * aiChoice.length)
+ return RNG = aiChoice[Math.floor(Math.random() * aiChoice.length)]
 }
 
 //Calls AI for choice
 const computerSelection = computerPlay()
-console.log("Ai has chosen " + computerPlay())
 
 
- //Plays one round of the game
- function playRound(playerSelection, computerSelection){
-   if(playerSelection === computerSelection){
-    return "Tie!";
-   }else if(playerSelection === "paper"){
-     if(computerSelection === "rock"){
-       return "You win"
-     }else{
-       return "You loose"
-     }
-     }else if(playerSelection === "rock"){
-       if(computerSelection === "scissors"){
-         return "You win"
-       }else{
-         return "You loose"
-       }
-        }else if (playerSelection === "scissors"){
-            if (computerSelection === "paper"){
-                return "Win";
-            } else {
-                return "Loss";
-            }
-        } 
-    }
-
+ //Plays one round of the game through conditional logic
+ function playRound(playerSelection, computerSelection) {
+  if (computerSelection.toLowerCase() === playerSelection.toLowerCase()) {
+     console.log("It's a draw!");
+     return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+  } else if (computerSelection.toLowerCase() === "rock" && playerSelection.toLowerCase() === "scissors") {
+     console.log("Rock beats scissors! You lose :(");
+     return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+  } else if (computerSelection.toLowerCase() === "scissors" && playerSelection.toLowerCase() === "rock") {
+     console.log("Rock beats scissors! You win :D");
+     return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+  } else if (computerSelection.toLowerCase() === "scissors" && playerSelection.toLowerCase() === "paper") {
+     console.log("Scissors beats paper! You lose :(");
+     return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+  } else if (computerSelection.toLowerCase() === "paper" && playerSelection.toLowerCase() === "scissors") {
+     console.log("Scissors beats paper! You win :D");
+     return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+  } else if (computerSelection.toLowerCase() === "paper" && playerSelection.toLowerCase() === "rock") {
+     console.log("Paper beats rock! You lose :(");
+     return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+  } else if (computerSelection.toLowerCase() === "rock" && playerSelection.toLowerCase() === "paper") {
+     console.log("Paper beats rock! You win :D");
+     return("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+  } else {
+     console.log("Something went wrong, please try again");
+  }
+    
+  }
         
 
 
