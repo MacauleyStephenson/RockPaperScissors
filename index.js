2081872635e1e3fae8720d5e7fcbccf2bab37f6b
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-06-07 12:06:48 +0100

    added functionality for reset button

--- index.js
+++ index.js
@@ -13,36 +13,47 @@ function computerPlay() {
 
 //Plays one round of the game through conditional logic
 function playRound(playerSelection, computerSelection) {
-	if (computerSelection === playerSelection) {
+	if (playerSelection === computerSelection) {
 		return "tie!";
-	} else if (computerSelection === "rock" && playerSelection === "scissors") {
-		console.log("Rock beats scissors! You lose :(");
-		return ("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
-	} else if (computerSelection === "scissors" && playerSelection === "rock") {
-		console.log("Rock beats scissors! You win :D");
-		return ("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
-	} else if (computerSelection === "scissors" && playerSelection === "paper") {
-		console.log("Scissors beats paper! You lose :(");
-		return ("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
-	} else if (computerSelection === "paper" && playerSelection === "scissors") {
-		console.log("Scissors beats paper! You win :D");
-		return ("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
-	} else if (computerSelection === "paper" && playerSelection === "rock") {
-		console.log("Paper beats rock! You lose :(");
-		return ("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
-	} else if (computerSelection === "rock" && playerSelection === "paper") {
-		console.log("Paper beats rock! You win :D");
-		return ("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+	} else if (playerSelection === "rock" && computerSelection === "scissors") {
+		return "Rock beats scissors! You lose :(";
+	} else if (playerSelection === "scissors" && computerSelection === "rock") {
+		return "Rock beats scissors! You win :D";
+	} else if (playerSelection === "scissors" && computerSelection === "paper") {
+		return "Scissors beats paper! You lose :(";
+	} else if (playerSelection === "paper" && computerSelection === "scissors") {
+		return "Scissors beats paper! You win :D";
+	} else if (playerSelection === "paper" && computerSelection === "rock") {
+		return "Paper beats rock! You lose :(";
+	} else if (playerSelection === "rock" && computerSelection === "paper") {
+		return "Paper beats rock! You win :D";
 	} else {
 		return "Something went wrong, please try again"
 	}
 
 }
 
-
+let player = 0;
+let computer = 0;
 
 // //returns variables to play the game
 // playRound(playerSelection, computerSelection)
 // computerPlay()
 
 // console.log(playRound(playerSelection, computerSelection));
+
+const btnReset = document.querySelector('.reset')
+
+btnReset.addEventListener('click', function () {
+	player = 0;
+	computer = 0;
+	rockBtn.disabled = false;
+	paperBtn.disabled = false;
+	scissorskBtn.disabled = false;
+	divResultsFinal.textContent = '';
+	divResultsPlayer.textContent = `: ${player}`;
+	divResultsPc.textContent = `: ${computer} `;
+	boxVs.style.outlineColor = '#FFFFFF';
+	title.style.color = '#FFFFFF';
+	resultFinal.style.color = '#FFFFFF';
+})
